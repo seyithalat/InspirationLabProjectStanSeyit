8a14ec26ed4ca62057be876a1ceb4374d996f30c
seyithalat <r1059309@student.thomasmore.be>
2025-04-15 19:34:34 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,85 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InspirationLabProjectStanSeyit
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // ?? Carousel images
+        private readonly List<string> imagePaths = new List<string>
+        {
+            "Images/homepagecarouselimage.jpg",
+            "Images/featurescarouselimage.jpg",
+            "Images/profilecarouselimage.jpg",
+            "Images/plannercarouselimage.jpg",
+            "Images/groupscarouselimage.jpg",
+            "Images/gamescarouselimage.jpg",
+            "Images/notescarouselimage.jpg",
+            "Images/managementcarouselimage.jpg",
+            "Images/contactcarouselimage.jpg"
+        };
+
+        private int startIndex = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            Application.Current.MainWindow = this;
+            Loaded += Window_Loaded;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Hide();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var registerWindow = new RegisterWindow();
+            registerWindow.Show();
+            this.Hide();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateImageSet();
+        }
+
+        private void UpdateImageSet()
+        {
+            if (imagePaths.Count < 3)
+            {
+                MessageBox.Show("You need at least 3 images for the carousel to work properly.", "Image Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                Image1.Source = new BitmapImage(new Uri(imagePaths[startIndex % imagePaths.Count], UriKind.Relative));
+                Image2.Source = new BitmapImage(new Uri(imagePaths[(startIndex + 1) % imagePaths.Count], UriKind.Relative));
+                Image3.Source = new BitmapImage(new Uri(imagePaths[(startIndex + 2) % imagePaths.Count], UriKind.Relative));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load images. Make sure they exist in the Images folder.\n\n" + ex.Message,
+                                "Load Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void PrevImage_Click(object sender, RoutedEventArgs e)
+        {
+            startIndex = (startIndex - 1 + imagePaths.Count) % imagePaths.Count;
+            UpdateImageSet();
+        }
+
+        private void NextImage_Click(object sender, RoutedEventArgs e)
+        {
+            startIndex = (startIndex + 1) % imagePaths.Count;
+            UpdateImageSet();
         }
     }
 }
---
+++ RegisterWindow.xaml.cs
@@ -0,0 +1,31 @@
+using System.Windows;
+
+namespace InspirationLabProjectStanSeyit
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string email = EmailBox.Text;
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please fill in all fields.", "Missing Info", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            MessageBox.Show($"Thanks for registering, {username}!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            var loginWindow = new LoginWindow();
+            Application.Current.MainWindow.Show(); 
+            this.Close();                          
+        }
+    }
+}
